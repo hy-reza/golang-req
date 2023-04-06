875c971b074314ff89cad7a7ffc975f05d21eb76
hy-reza <hy.rezaalfanda@gmail.com>
2023-04-06 19:38:23 +0700

    Done

--- main.go
+++ main.go
@@ -10,11 +10,13 @@ import (
 	"time"
 )
 
+// Data struct untuk menyimpan nilai water dan wind
 type Data struct {
 	Water int `json:"water"`
 	Wind  int `json:"wind"`
 }
 
+// getStatus mengembalikan status berdasarkan nilai dan ambang batas yang diberikan
 func getStatus(value int, thresholds []int, statuses []string) string {
 	for i, threshold := range thresholds {
 		if value <= threshold {
@@ -25,19 +27,26 @@ func getStatus(value int, thresholds []int, statuses []string) string {
 }
 
 func main() {
+	// URL untuk mengirim POST request
 	url := "https://jsonplaceholder.typicode.com/posts"
 
+	// Mulai loop yang akan berjalan setiap 15 detik
 	for {
+		// Generate nilai acak untuk water dan wind antara 1 dan 100
 		water := rand.Intn(100) + 1
 		wind := rand.Intn(100) + 1
+
+		// Buat objek data dengan nilai water dan wind yang dihasilkan
 		data := Data{Water: water, Wind: wind}
 
+		// Marshaling data ke dalam format JSON
 		jsonData, err := json.Marshal(data)
 		if err != nil {
 			fmt.Println("error:", err)
 			return
 		}
 
+		// Mengirim POST request dengan data JSON
 		resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
 		if err != nil {
 			fmt.Println("error:", err)
@@ -45,15 +54,19 @@ func main() {
 		}
 		defer resp.Body.Close()
 
+		// Membaca response dari POST request
 		body, _ := io.ReadAll(resp.Body)
 		fmt.Println("POST Response:", string(body))
 
+		// Mendapatkan status water dan wind berdasarkan nilai yang dihasilkan
 		waterStatus := getStatus(water, []int{5, 8}, []string{"aman", "siaga", "bahaya"})
 		windStatus := getStatus(wind, []int{6, 15}, []string{"aman", "siaga", "bahaya"})
 
+		// Menampilkan status water dan wind
 		fmt.Printf("status water : %s\n", waterStatus)
 		fmt.Printf("status wind : %s\n", windStatus)
 
+		// Tunggu 15 detik sebelum melanjutkan loop
 		time.Sleep(15 * time.Second)
 	}
 }
